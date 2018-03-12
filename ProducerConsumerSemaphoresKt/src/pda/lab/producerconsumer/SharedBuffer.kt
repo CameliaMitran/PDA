84884cd714b4d0dcfc76516e27cb0709e82a733a
georgianapopescu <32480006+georgianapopescu@users.noreply.github.com>
2018-03-12 08:41:07 +0200

    Update SharedBuffer.kt

--- ProducerConsumerSemaphoresKt/src/pda/lab/producerconsumer/SharedBuffer.kt
+++ ProducerConsumerSemaphoresKt/src/pda/lab/producerconsumer/SharedBuffer.kt
@@ -10,14 +10,28 @@ class SharedBuffer(@Volatile var bufferSize: Int,
     //val for immutable properties
     private val bufferList = ArrayList<String>()
 
-    fun put(message: String) {
+    fun put(message: String) = synchronized(lock){
+        while (isFull()){
+            print(message)
+            lock.wait()
+        }
+        //Thread.sleep(Random().nextInt(100).toLong())
         bufferList.add(message)
+        println("$message was added to the buffer.")
+        lock.notifyAll()
     }
 
     @Synchronized
-    fun get(): String {
+    fun get(): String = synchronized(lock){
         val message: String
+        while (isEmpty()){
+            println("Buffer is empty; waiting for producer")
+            lock.wait()
+        }
         message = bufferList.removeAt(0)
+        println("$message was removed from buffer.")
+        lock.notifyAll()
+        return message
     }
 }
 
