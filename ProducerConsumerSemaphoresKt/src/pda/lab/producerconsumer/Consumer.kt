815e5b9a3cc1699eab86afcd0e63bf1e3c18c944
georgianapopescu <32480006+georgianapopescu@users.noreply.github.com>
2018-03-12 08:57:11 +0200

    Update Consumer.kt

--- ProducerConsumerSemaphoresKt/src/pda/lab/producerconsumer/Consumer.kt
+++ ProducerConsumerSemaphoresKt/src/pda/lab/producerconsumer/Consumer.kt
@@ -4,9 +4,9 @@ class Consumer(private val buffer: SharedBuffer) : Thread(){
 
     override fun run() {
         while(true){
-            buffer.semaphoreFull.release()//--
+            buffer.semaphoreFull.acquire()//--
             val item = buffer.get()
-            buffer.semaphoreFree.acquire()//++
+            buffer.semaphoreFree.release()//++
             println(item+ " was removed from the buffer")
         }
     }
